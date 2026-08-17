bb2beb8f9af9fae89f9d1e22f20d30d91456117c
geogeoN <futbolyer@gmail.com>
2026-08-17 23:50:05 +0300

    fizzbuzz mod

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,8 +1,8 @@
 function fizzBuzz(n) {
-    if (n % 3 === 0 && n % 5 === 0) return 'FizzBuzz';
+    if (n % 3 === 0 && n % 5 === 0) console.log('FizzBuzz');
     else {
-        if (n % 3 === 0) return 'Fizz';
-        else if (n % 5 === 0) return 'Buzz';
-        else return 'Ничего';
+        if (n % 3 === 0) console.log('Fizz')
+        else if (n % 5 === 0) console.log('Buzz');
+        else console.log('Ничего');
     }
 }
